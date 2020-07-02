14f7223734daab972cfaa9c11dc52a1efd670249
rashmiranganath <rashmi18@navgurukul.org>
2020-07-02 15:54:01 +0530

    npm-github

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const shell = require('shelljs')
+const path = '///home/rashmu/Desktop/'
+// const repo =  'https://github.com/rashmiranganath/request-api'
+shell.cd(path)
+shell.exec('git clone https://github.com/rashmiranganath/request-api')
+
+
+
+
+
